4b3a7319f12827495ec68bb5422fb448ffcd14a1
Alex Auritt <alexauritt@gmail.com>
2013-01-10 01:15:20 -0500

    initial quiz

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -9,5 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface QuizViewController : UIViewController
+{
+    int currentQuestionIndex;
+    
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
 
+- (IBAction)showQuestion:(id)sender;
+- (IBAction)showAnswer:(id)sender;
 @end
